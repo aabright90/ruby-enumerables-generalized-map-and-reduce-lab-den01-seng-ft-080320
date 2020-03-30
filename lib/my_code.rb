303a886c4b962170684f4e1b5344671aed9fe98c
Austen Bright <pureaustenbright@gmail.com>
2020-03-30 00:43:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
 # Your Code Here
+def map(array)
+  new_arr = []
+  i = 0
+  while i < array.length
+    new_arr << yield(array[i])
+    i += 1
+  end
+  new_arr
+end
+
+def reduce(array, starting_point = "0")
+  total = starting_point
+  i = 0
+  while i < array.length
+    total += yield(array[i])
+  end
+total
+end
